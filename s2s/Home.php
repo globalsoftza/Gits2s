75811d816ad53e72af47b4deafc7fb1bf03d964e
Imoyeh <s213444844@nmmu.ac.za>
2017-05-26 14:00:04 +0200

    Collins makes changes to the last p paragraph

--- s2s/Home.php
+++ s2s/Home.php
@@ -23,6 +23,7 @@
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sed sapien turpis. Integer sollicitudin, metus at vestibulum eleifend, velit tellus elementum sapien, vel cursus ex turpis eu ligula. Mauris elementum velit at lorem porttitor, eu elementum eros hendrerit. Nulla malesuada tortor ornare, euismod mi in, rhoncus leo. Cras mattis arcu in arcu semper, sit amet accumsan sem blandit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras ultrices sagittis velit nec dignissim. Nunc maximus commodo ultricies. Vestibulum placerat quam eget laoreet laoreet. Etiam at maximus lacus. In quis molestie mauris. In consectetur et lacus ut ullamcorper. Mauris porta luctus suscipit. Curabitur fermentum bibendum pellentesque. Mauris sit amet eleifend urna, vitae eleifend arcu.
         </div>
 		<p>
+Collins Imoyeh
 		</p>
     </body>
 </html>
